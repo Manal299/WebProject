3de84eee461e978171d72b3680ef0d9ff7e0a627
Manal Abdul Razzaq <137073554+Manal299@users.noreply.github.com>
2024-05-17 11:32:32 +0500

    backend

--- src/component/Footer/footer.tsx
+++ src/component/Footer/footer.tsx
@@ -5,11 +5,13 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-200 py-8 font-heebo">
       <div className="container mx-auto flex flex-wrap justify-between">
+        {/* Column 1 */}
         <div className="footer-column w-full sm:w-1/3">
           <p className="font-bold text-lg mb-2 font-garamond">Flower Shop</p>
           <p className="text-sm">Welcome to the world of Florist, where flowers come to life with love and creativity. Discover our story, our passion for flowers, and our commitment to making every moment memorable.</p>
         </div>
 
+        {/* Column 2 */}
         <div className="footer-column  w-full sm:w-1/3">
           <p className="font-bold text-lg ml-52 mb-2 font-garamond">Links</p>
           <ul className="text-sm ml-52">
@@ -20,6 +22,7 @@ const Footer: React.FC = () => {
           </ul>
         </div>
 
+        {/* Column 3 */}
         <div className="footer-column w-full sm:w-1/3">
           <p className="font-bold text-lg mb-2 font-garamond">Contact Us</p>
           <p className="text-sm">
@@ -30,11 +33,13 @@ const Footer: React.FC = () => {
         </div>
       </div>
 
+      {/* Social media icons */}
       <div className="flex justify-end items-center mt-4 mr-4">
         <FaInstagram className="text-2xl mr-2" />
         <FaFacebookSquare className="text-2xl" />
       </div>
 
+      {/* Copyright */}
       <div className="text-center mt-8 text-gray-800">
         <p className="text-sm">Copyright  &copy; 2024 Bloomila</p>
       </div>
--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../component/Header/header'; 
 import Homepage from '../component/Homepage/homepage';
 import Content from '../component/Content/content';
 import Footer from '../component/Footer/footer';
 import Slideshow from '../component/Slideshow/slideshow';
+import axios from 'axios';
 
 const MainPage = () => {
+  interface user {
+    name: string;
+    email: string;
+  }
+  const [data, setData] = useState<user[]>([])
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000');
+      console.log('Data fetched:', response.data);
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
   return (
+    // <div>
+    //   {data.map((dt,index)=>{
+    //     return(
+    //       <div key={index}>
+    //         <h1 className='text-black'>{dt.name}</h1>
+    //         <p className='text-black'>{dt.email}</p>
+    //       </div>
+    //     )
+    //   })}
+    // </div>
    <>
    <Header/>
    <Homepage/>
